2641e4fff616ecd02c51539219f2863e8d7f5ae5
Lorenzoscaccia.- <scaccia.lorenzo08@gmail.com>
2026-02-18 10:32:55 +0100

    correzione

--- script.js
+++ script.js
@@ -16,16 +16,29 @@ function draw() {
   let colWidth=width/3;
   let bottomHeight=height-topHeight;
   
-  //colonna1/2/3
-  stroke("black");
-  strokeWeight(2);
-  fill("lightgray");
+  // colonne
+noStroke();
+fill("lightgray");
 
-  // 3 colonne
-  rect(0, topHeight, colWidth, bottomHeight);
-  rect(colWidth, topHeight, colWidth, bottomHeight);
-  rect(colWidth * 2, topHeight, colWidth, bottomHeight);
-}
-  
+rect(0, topHeight, colWidth, bottomHeight);
+rect(colWidth, topHeight, colWidth, bottomHeight);
+rect(colWidth * 2, topHeight, colWidth, bottomHeight);
 
+// linee bordi
+stroke("black");
+strokeWeight(2);
+
+// bordo sinistro
+line(0, topHeight, 0, height);
+
+// separatori verticali
+line(colWidth, topHeight, colWidth, height);
+line(colWidth * 2, topHeight, colWidth * 2, height);
+
+// bordo destro
+line(width - 1, topHeight, width - 1, height);
+
+// linea sopra le colonne
+line(0, topHeight, width, topHeight);
 }
+
