aadffff3e7d9b2834726898cee24d1ab1adac07f
Lorenzoscaccia.- <scaccia.lorenzo08@gmail.com>
2026-02-18 09:29:40 +0100

    divisione in 3 colonne con spazio per inserire dimensione processi

---
+++ script.js
@@ -0,0 +1,29 @@
+let topHeight=120;
+
+function setup() {
+  createCanvas(900, 600);
+}
+
+function draw() {
+    background(220);
+
+  //bianco sopra
+  fill(255);
+  noStroke();
+  rect(0,0,width,topHeight);
+  
+  //sotto 3 colonne
+  let colWidth=width/3;
+  let bottomHeight=height-topHeight;
+  //colonna1
+  fill(255,0,0);
+  rect(0,topHeight,colWidth,bottomHeight);
+  
+  //colonna 2
+  fill(0,255,0);
+  rect(colWidth,topHeight,colWidth,bottomHeight);
+  
+  //colonna3
+  fill(0,0,255);
+  rect(colWidth*2,topHeight,colWidth,bottomHeight);
+}
